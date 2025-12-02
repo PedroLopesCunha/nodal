9e2d3092f87ac60acf72b0837807bc6c7f331899
Pedro Cunha <pedromplcunha@gmail.com>
2025-12-02 11:37:20 +0000

    Added customer model with devise, make sure it is not possible to sign up if not memeber

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_02_105034) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_02_112223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,24 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_02_105034) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.bigint "organisation_id", null: false
+    t.string "company_name"
+    t.string "contact_name"
+    t.string "contact_phone"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["organisation_id"], name: "index_customers_on_organisation_id"
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "members", force: :cascade do |t|
@@ -80,6 +98,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_02_105034) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "customers", "organisations"
   add_foreign_key "org_members", "members"
   add_foreign_key "org_members", "organisations"
 end
