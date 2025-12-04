626c7455d7c65e91581e17c5f0494595eb44cf54
Yannik Caspar <y.caspar@bluewin.ch>
2025-12-04 19:01:32 +0100

    setup for development mail sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,14 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'pedro@gmail.com'
+  config.mailer_sender = 'welcome@nodal.com'
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
